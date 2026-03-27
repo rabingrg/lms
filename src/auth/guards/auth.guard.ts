d38ad4b3278c8731ef2a43ca2cd3fb26075a7ac3
rabingrg <wrabeeng@gmail.com>
2026-03-27 12:39:58 +0545

    feat(authentication): auth guard setup

---
+++ src/auth/guards/auth.guard.ts
@@ -0,0 +1,56 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtPayload, verify } from 'jsonwebtoken';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      throw new UnauthorizedException('Missing authorization header');
+    }
+
+    const parts: string[] = authHeader.split(' ');
+    if (parts.length !== 2 || parts[0] !== 'Bearer') {
+      throw new UnauthorizedException('Invalid token format!');
+    }
+
+    const token = parts[1];
+    if (!token) {
+      throw new UnauthorizedException('Missing token!');
+    }
+
+    try {
+      const decode = verify(token, process.env.JWT_SECRET!) as JwtPayload;
+      console.log({ decode });
+      if (!decode.sub) {
+        throw new UnauthorizedException('Invalid token payload!');
+      }
+      return true;
+    } catch (error) {
+      if (error instanceof UnauthorizedException) {
+        throw error;
+      }
+
+      // jwt specific errors
+      if (error instanceof Error) {
+        if (error.message === 'jwt expired') {
+          throw new UnauthorizedException('Token expired');
+        } else if (
+          error.message === 'invalid token' ||
+          error.message === 'invalid signature'
+        ) {
+          throw new UnauthorizedException('Invalid token');
+        }
+      }
+
+      throw new UnauthorizedException('Unauthorized');
+    }
+  }
+}
