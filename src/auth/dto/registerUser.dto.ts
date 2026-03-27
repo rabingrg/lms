8dd62c788f68dd1a046ef9623619bed4ec73f109
rabingrg <wrabeeng@gmail.com>
2026-03-27 10:02:57 +0545

    feat(register): added validation

--- src/auth/dto/registerUser.dto.ts
+++ src/auth/dto/registerUser.dto.ts
@@ -1,6 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class RegisterDto {
+  @IsString()
+  @IsNotEmpty()
   firstName: string;
+
+  @IsString()
   lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @MinLength(6)
   password: string;
 }
